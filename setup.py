d90fca3845b606b1f438063deda4af31fedfa8c6
zachpinto <zachwalker.pinto@gmail.com>
2023-02-14 20:24:02 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Economic analysis of GDP components by state from 1997-2021',
+    author='Zachary T.W. Pinto',
+    license='MIT',
+)
